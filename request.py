e54f6ae0eb2ae3fa25dd2201d1988d8f26f21418
ShrideviReddy <shridevi.reddy7j1992@gmail.com>
2020-08-14 01:00:35 -0400

    requirement update

--- request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/results'
-r = requests.post(url,json={'clump_thickness':5, 'uniform_cell_size':5,
-                            'uniform_cell_shape':6, 'marginal_adhesion':7,
-                            'single_epithelial_size':8, 'bare_nuclei':9,
-                            'bland_chromatin':5, 'normal_nucleoli':7, 'mitoses': 5})
-
-print(r.json())
